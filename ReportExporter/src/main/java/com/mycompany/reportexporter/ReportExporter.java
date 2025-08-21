7bf53690d1e61e661309f1392e99942104f81970
HowardKiat <buisnesskiat123@gmail.com>
2025-08-21 18:20:45 +0800

    Initial Commit

---
+++ ReportExporter/src/main/java/com/mycompany/reportexporter/ReportExporter.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reportexporter;
+
+/**
+ *
+ * @author User
+ */
+public class ReportExporter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
